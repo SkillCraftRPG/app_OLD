4c479f39486d3282337f5226a0da22fde9143501
Francis Pion <francispion@hotmail.com>
2024-05-06 23:45:36 -0400

    Implemented SignInPayload validation.

---
+++ backend/src/SkillCraft.Application/Accounts/Validators/CredentialsValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using Logitar.Identity.Domain.Users;
+using SkillCraft.Contracts.Accounts;
+
+namespace SkillCraft.Application.Accounts.Validators;
+
+internal class CredentialsValidator : AbstractValidator<Credentials>
+{
+  public CredentialsValidator()
+  {
+    RuleFor(x => x.EmailAddress).NotEmpty().MaximumLength(EmailUnit.MaximumLength).EmailAddress();
+    When(x => x.Password != null, () => RuleFor(x => x.Password).NotEmpty());
+  }
+}
