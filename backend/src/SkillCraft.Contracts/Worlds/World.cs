75ea13f2aefbb412c3f1d21c5ce2bed173bf1d91
Francis Pion <francispion@hotmail.com>
2024-05-07 14:56:10 -0400

    Contracts.

---
+++ backend/src/SkillCraft.Contracts/Worlds/World.cs
@@ -0,0 +1,21 @@
+﻿using Logitar.Portal.Contracts;
+
+namespace SkillCraft.Contracts.Worlds;
+
+public class World : Aggregate
+{
+  public string UniqueSlug { get; set; }
+  public string? DisplayName { get; set; }
+  public string? Description { get; set; }
+
+  public World() : this(string.Empty)
+  {
+  }
+
+  public World(string uniqueSlug)
+  {
+    UniqueSlug = uniqueSlug;
+  }
+
+  public override string ToString() => $"{DisplayName ?? UniqueSlug} | {base.ToString()}";
+}
